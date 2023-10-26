5c370a0bb0993cbf8f8771f895fb220e275ac2ea
Charles Farquharson <hello@cfarq.co.uk>
2023-10-26 14:36:51 +0200

    add better backend solution + redux product api

--- src/components/Page/PageHeader.tsx
+++ src/components/Page/PageHeader.tsx
@@ -5,12 +5,12 @@ import { useSelector } from "react-redux";
 export const PageHeader = ({}): JSX.Element => {
   const [cartSize, setCartSize] = useState(0);
 
-  const cart = useSelector((state) => state.cart);
+  // const cart = useSelector((state) => state.cart);
 
-  useEffect(() => {
-    console.log(cart.length);
-    setCartSize(cart.length);
-  }, [cart]);
+  // useEffect(() => {
+  //   console.log(cart.length);
+  //   setCartSize(cart.length);
+  // }, [cart]);
 
   return (
     <div className="flex items-center justify-between w-full fixed top-0 shadow-xl bg-slate-700 text-white px-10 py-5">
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,20 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
 
-import store from "./redux/store";
+import productsReducer, { productsFetch } from "./redux/productsSlice";
+import { productsApi } from "./redux/productsApi";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+store.dispatch(productsFetch());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
