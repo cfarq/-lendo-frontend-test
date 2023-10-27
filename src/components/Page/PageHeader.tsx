367568fdd353a649afe26bc926c1f730fe357794
Charles Farquharson <hello@cfarq.co.uk>
2023-10-27 18:35:39 +0200

    final mobile tidy up

--- src/components/Page/PageHeader.tsx
+++ src/components/Page/PageHeader.tsx
@@ -13,7 +13,7 @@ export const PageHeader = ({}): JSX.Element => {
   }, [cartTotalQuantity]);
 
   return (
-    <div className="flex items-center justify-between w-full fixed top-0 shadow-xl bg-slate-700 text-white px-10 py-5">
+    <div className="flex items-center justify-between w-full fixed z-20 top-0 shadow-xl bg-slate-700 text-white px-10 py-5">
       <div>
         <Link to="/">All products</Link>
       </div>
